84ba9e183605657e1d5bad26670096e10c1a0d13
Nabson Paiva <nabsonp@gmail.com>
2019-05-05 12:29:43 -0400

    Análise de memória feita

--- Trabalho 02/questao03.c
+++ Trabalho 02/questao03.c
@@ -4,15 +4,23 @@
 #include "abp.h"
 #include "vetor.h"
 int main() {
-    int tam = 1000000, vet[tam], tamBusca = 30, busca[tamBusca], j;
+    int tam = 1000000, vet[tam], tamBusca = 30, busca[tamBusca], j,e;
     float tempoVet[tamBusca], tempoABP[tamBusca], tempoMedioVet = 0, tempoMedioABP = 0;
     clock_t t0;
 
+    printf("Início do processo: 64 KB ");
+    scanf("%d", &e);
+
     gerarVetorDesordenado(tam, vet);
     gerarVetPesquisa(tamBusca,busca,tam,vet);
+    printf("Vetor de 1M e de busca criados: 3.9 MB ");
+    scanf("%d", &e);
+
     no *r = criarArvore();
     r = converterVetorABP(r,tam,vet);
-    printf("a\n");
+    printf("Árvore criada com elementos do vetor: 34,3 MB ");
+    scanf("%d", &e);
+
     quickSort(tam,vet);
     for (j=0; j<tamBusca; j++) {
       // Busca no vetor
@@ -41,5 +49,8 @@ int main() {
       printf("%fs ", tempoABP[j]);
     }
     printf("\nTempo médio: %fs\n", tempoMedioABP);
+
+    printf("\nFIM ");
+    scanf("%d", &e);
     return 0;
 }
