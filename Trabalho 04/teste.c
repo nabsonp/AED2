9e1d11128a5c0118067a20e5753435ad87481a00
Nabson Paiva <nabsonp@gmail.com>
2019-06-26 13:35:47 -0400

    Variação do tamanho de grafo

--- Trabalho 04/teste.c
+++ Trabalho 04/teste.c
@@ -2,21 +2,57 @@
 #include "grafo.h"
 
 int main() {
-  int tam = 5;
   float conec;
-
-  int grafo[tam][tam];
   int n, i;
+
+  // Grafo 1
+  int tam = 4;
+  int grafo1[tam][tam];
+  printf("************************ GRAFO 1 **************************\n");
   for (i=1; i<5; i++) {
     conec = 0.25*i;
-    n = criarGrafo(tam,grafo,conec);
-    mostrarGrafo(tam,grafo);
+    n = criarGrafo(tam,grafo1,conec);
+    mostrarGrafo(tam,grafo1);
     printf("\t -> %d arestas setadas (%.1f%% de conectividade).\n\n", n, conec*100);
     printf("\t------------- DFS -------------");
-    char ciclo = DFS(tam,grafo,0);
+    char ciclo = DFS(tam,grafo1,0);
     printf("\n\t   -> Ciclo? %c",ciclo);
     printf("\n\n\t------------- BFS -------------");
-    BFS(tam,grafo,0);
+    BFS(tam,grafo1,0);
+    printf("\n\n");
+  }
+
+  // Grafo 2
+  tam = 5;
+  int grafo2[tam][tam];
+  printf("************************ GRAFO 2 **************************\n");
+  for (i=1; i<5; i++) {
+    conec = 0.25*i;
+    n = criarGrafo(tam,grafo2,conec);
+    mostrarGrafo(tam,grafo2);
+    printf("\t -> %d arestas setadas (%.1f%% de conectividade).\n\n", n, conec*100);
+    printf("\t------------- DFS -------------");
+    char ciclo = DFS(tam,grafo2,0);
+    printf("\n\t   -> Ciclo? %c",ciclo);
+    printf("\n\n\t------------- BFS -------------");
+    BFS(tam,grafo2,0);
+    printf("\n\n");
+  }
+
+  // Grafo 3
+  tam = 6;
+  int grafo3[tam][tam];
+  printf("************************ GRAFO 3 **************************\n");
+  for (i=1; i<5; i++) {
+    conec = 0.25*i;
+    n = criarGrafo(tam,grafo3,conec);
+    mostrarGrafo(tam,grafo3);
+    printf("\t -> %d arestas setadas (%.1f%% de conectividade).\n\n", n, conec*100);
+    printf("\t------------- DFS -------------");
+    char ciclo = DFS(tam,grafo3,0);
+    printf("\n\t   -> Ciclo? %c",ciclo);
+    printf("\n\n\t------------- BFS -------------");
+    BFS(tam,grafo3,0);
     printf("\n\n");
   }
   return 0;
